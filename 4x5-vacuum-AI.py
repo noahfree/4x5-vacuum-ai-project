f275981c5793280e0d17daa6c2586b731d123651
noahfree <nsfq94@mail.missouri.edu>
2022-09-22 13:27:54 -0500

    add comments to code

--- 4x5-vacuum-AI.py
+++ 4x5-vacuum-AI.py
@@ -1,7 +1,10 @@
+# Noah Free and Isaac Schroeder
+
 from enum import Enum
 import copy
 import time
 
+# Action enum containing the cost of each possible move
 class Action(float, Enum):
     LEFT = 1.0
     RIGHT = 0.9
@@ -9,14 +12,17 @@ class Action(float, Enum):
     DOWN = 0.7
     SUCK = 0.6
 
+# Coordinates class containing X and Y values
 class Coordinates:
     def __init__(self, x, y):
         self.X = x
         self.Y = y
 
+# class Env is the 4x5 grid with a vacuum
 class Env:
     def __init__(self, x, y):
         matrix=[]
+		# a 4x5 matrix is initialized with the string 'clean'
         for i in range(4):
             row=[]
             for j in range(5):
@@ -27,7 +33,9 @@ class Env:
         self.total_cost = 0
         self.rooms_cleaned = 0
         self.path = []
+		# layer is only used in the iterative deepening search
         self.layer = -1
+	# set_dirt is used to create the initial environment
     def set_dirt(self, x, y):
         self.matrix[x][y] = 'dirty'
     def get_X(self):
@@ -36,6 +44,7 @@ class Env:
         return self.position.Y
     def get_cost(self):
         return self.total_cost
+	# the following 5 functions are called when the vacuum does a given action; the vacuum's position is altered, the total cost is updated, and the path is updated
     def move_left(self):
         self.position.Y -= 1
         self.total_cost += Action.LEFT.value
@@ -59,9 +68,11 @@ class Env:
             self.matrix[self.position.X][self.position.Y] = 'clean'
         self.path.append('SUCK')
 
+# function compare_states() takes a node and a list of nodes; true is returned if the state of the given node is the same as the state of one of the nodes in the list
 def compare_states(node, closed):
     toggle = True
     for state in closed:
+		# if X or Y of the vacuum are not the same, then the states are not identical
         if node.get_X() != state.get_X():
             continue
         if node.get_Y() != state.get_Y():
@@ -70,24 +81,30 @@ def compare_states(node, closed):
             for j in range(len(node.matrix[i])):
                 if (node.matrix[i][j] != state.matrix[i][j]):
                     toggle = False
+		# if toggle is true, then a node with identical state has been found, so True is returned
         if toggle:
             return True
     return False
 
+# print_state is used to print the state of the given node
 def print_state(state):
     print("vacuum at: " + str(state.position.X) + ", " + str(state.position.Y) + "\n")
     for i in state.matrix:
         print(i[0] + " " + i[1] + " " + i[2] + " " + i[3] + " " + i[4])
         print("")
 
+# expand_node is used to expand the given node/state; node expansion is optimized to prevent the vacuum from undoing the action that it just performed. For example, the vacuum cannot move Left if it moved Right in its previous move. Additionally, the number of nodes generated is returned
 def expand_node(state, fringe, layer):
+	# the generated count is initialized to 0
     generated_count = 0
     if (state.matrix[state.get_X()][state.get_Y()] == 'dirty'):
         new_state = copy.deepcopy(state)
         new_state.clean_room()
         new_state.layer = layer
         fringe.append(new_state)
+		# if the room is dirty, the only possible move is to clean the room, so 1 is returned since only 1 node has been generated
         return 1
+	# these if statements prevent the vacuum from moving outside of the grid, and the vacuum is also prevented from returning to its previous state
     if (state.get_Y() != 0 and (len(state.path) == 0 or state.path[len(state.path) - 1] != 'RIGHT')):
         new_state = copy.deepcopy(state)
         new_state.move_left()
@@ -112,46 +129,65 @@ def expand_node(state, fringe, layer):
         new_state.layer = layer
         fringe.append(new_state)
         generated_count += 1
+	# the number of nodes generated is returned to the caller
     return generated_count
 
+# select_node() is called to return the node in the given list that has the lowest total cost
 def select_node(fringe):
 	selected = 0
 	for i in range(len(fringe)):
 		if fringe[i].total_cost < fringe[selected].total_cost:
 			selected = i
+		# if the two nodes have the same cost, the tie breaker is based on which has the lower X coordinate, followed by which has the lower Y coordinate
+		if fringe[i].total_cost == fringe[selected].total_cost:
+			if fringe[i].get_X() < fringe[selected].get_X():
+				selected = i
+			if fringe[i].get_X() == fringe[selected].get_X() and fringe[i].get_Y() < fringe[selected].get_Y():
+				selected = i
 	return selected
 
-def uniform_graph_search(state, fringe, num_of_dirty_rooms):
+# uniform_graph_search is used to run the uniform cost graph search algorithm on the given state until it cleans num_of_dirty_rooms rooms
+def uniform_graph_search(state, num_of_dirty_rooms):
     expanded_count = 0
     generated_count = 0
     goal = []
+	fringe = []
     closed = []
     fringe.append(state)
     while len(fringe) > 0:
         node = fringe.pop(select_node(fringe))
+		# when a solution is found, the generated and expanded counts are returned, and the goal node is returned
         if (node.rooms_cleaned == num_of_dirty_rooms):
             print("   Generated Count => " + str(generated_count))
             print("   Expanded Count => " + str(expanded_count))
             return node
+		# compare_states is called to see if the current node has already been expanded
         if (not compare_states(node, closed)):
             closed.append(node)
+			# generated_count and expanded_count are incremented accordingly
             generated_count += expand_node(node, fringe, -1)
             expanded_count += 1
+	# none is returned if a goal was not able to be found
     return None
 
-def uniform_tree_search(state, fringe, num_of_dirty_rooms):
+# uniform_tree_search is used to run the uniform cost tree search algorithm on the given state until if cleans num_of_dirty_rooms rooms
+def uniform_tree_search(state, num_of_dirty_rooms):
     expanded_count = 0
     generated_count = 0
     goal = []
+	fringe = []
     fringe.append(state)
     while len(fringe) > 0:
         node = fringe.pop(select_node(fringe))
+		# when a solution is found, the generated and expanded counts are returned, and the goal node is returned
         if (node.rooms_cleaned == num_of_dirty_rooms):
             print("   Generated Count => " + str(generated_count))
             print("   Expanded Count => " + str(expanded_count))
             return node
+		# the generated count and expanded count are incremented accordingly
         generated_count += expand_node(node, fringe, -1)
         expanded_count += 1
+	# none is returned if a goal was not able to be found
     return None
 
 class NodeStats:
@@ -191,6 +227,7 @@ def dls(node, num_of_dirty_rooms, limit, node_stats):
     if cutoff_occurred:
         return -1
 
+# print_info is used to print the goal path, cost of traversal, and execution time of the inputted node
 def print_info(goal, start_time, end_time):
     print('   Goal path =>', end=' ')
     if goal != None:
@@ -202,6 +239,7 @@ def print_info(goal, start_time, end_time):
 
 
 def main():
+	# while loop is used to create a menu system for the user
     while True:
         print("\n\nChoose algorithm to run:")
         print("  (A) Instance 1, Uniform Tree Search")
@@ -215,71 +253,84 @@ def main():
 
         if (selection == 'A' or selection == 'a'):
             print('\nInstance 1 - Uniform Tree Search:')
+			# instance 1's environment is set up, indexed at zero so each coordinate is decreased by 1
             instance_1 = Env(1, 1)
             instance_1.set_dirt(0, 1)
             instance_1.set_dirt(1, 3)
             instance_1.set_dirt(2, 4)
+			# time() is called to calculate the execution time of the search function
             start = time.time()
-            goal = uniform_tree_search(instance_1, [], 3)
+            goal = uniform_tree_search(instance_1, 3)
             end = time.time()
             print_info(goal, start, end)
         elif (selection == 'B' or selection == 'b'):
             print('\nInstance 1 - Uniform Graph Search:')
+			# instance 1's environment is set up, indexed at zero so each coordinate is decreased by 1
             instance_1 = Env(1, 1)
             instance_1.set_dirt(0, 1)
             instance_1.set_dirt(1, 3)
             instance_1.set_dirt(2, 4)
+			# time() is called to calculate the execution time of the search function
             start = time.time()
-            goal = uniform_graph_search(instance_1, [], 3)
+            goal = uniform_graph_search(instance_1, 3)
             end = time.time()
             print_info(goal, start, end)
         elif (selection == 'C' or selection == 'c'):
             print('\nInstance 1 - Iterative Deepening Search:')
+			# instance 1's environment is set up, indexed at zero so each coordinate is decreased by 1
             instance_1 = Env(1, 1)
             instance_1.set_dirt(0, 1)
             instance_1.set_dirt(1, 3)
             instance_1.set_dirt(2, 4)
+			# time() is called to calculate the execution time of the search function
             start = time.time()
             goal = ids(instance_1, 3)
             end = time.time()
             print_info(goal, start, end)
         elif (selection == 'D' or selection == 'd'):
             print('\nInstance 2 - Uniform Tree Search:')
+			# instance 2's environment is set up, indexed at zero so each coordinate is decreased by 1
             instance_2 = Env(2, 1)
             instance_2.set_dirt(0, 1)
             instance_2.set_dirt(1, 0)
             instance_2.set_dirt(1, 3)
             instance_2.set_dirt(2, 2)
+			# time() is called to calculate the execution time of the search function
             start = time.time()
-            goal = uniform_tree_search(instance_2, [], 4)
+            goal = uniform_tree_search(instance_2, 4)
             end = time.time()
             print_info(goal, start, end)
         elif (selection == 'E' or selection == 'e'):
             print('\nInstance 2 - Uniform Graph Search:')
+			# instance 2's environment is set up, indexed at zero so each coordinate is decreased by 1
             instance_2 = Env(2, 1)
             instance_2.set_dirt(0, 1)
             instance_2.set_dirt(1, 0)
             instance_2.set_dirt(1, 3)
             instance_2.set_dirt(2, 2)
+			# time() is called to calculate the execution time of the search function
             start = time.time()
-            goal = uniform_graph_search(instance_2, [], 4)
+            goal = uniform_graph_search(instance_2, 4)
             end = time.time()
             print_info(goal, start, end)
         elif (selection == 'F' or selection == 'f'):
             print('\nInstance 2 - Iterative Deepening Search:')
+			# instance 2's environment is set up, indexed at zero so each coordinate is decreased by 1
             instance_2 = Env(2, 1)
             instance_2.set_dirt(0, 1)
             instance_2.set_dirt(1, 0)
             instance_2.set_dirt(1, 3)
             instance_2.set_dirt(2, 2)
+			# time() is called to calculate the execution time of the search function
             start = time.time()
             goal = ids(instance_2, 4)
             end = time.time()
             print_info(goal, start, end)
         elif (selection == 'G' or selection == 'g'):
+			print('\nExiting program...\n')
             break
         else:
             print("Invalid input: please enter a letter A-G.")
 
-
+# call main()
 main()
