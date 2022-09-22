22ba2ebd00170524919bd75658c107a150c8dbe8
noahfree <nsfq94@mail.missouri.edu>
2022-09-22 09:49:17 -0500

    Update 4x5-vacuum-AI.py

--- 4x5-vacuum-AI.py
+++ 4x5-vacuum-AI.py
@@ -1,5 +1,6 @@
 from enum import Enum
 import copy
+import time
 
 class Action(float, Enum):
     LEFT = 1.0
@@ -26,6 +27,7 @@ class Env:
         self.total_cost = 0
         self.rooms_cleaned = 0
         self.path = []
+        self.layer = -1
     def set_dirt(self, x, y):
         self.matrix[x][y] = 'dirty'
     def get_X(self):
@@ -72,7 +74,7 @@ def compare_states(node, closed):
             for j in range(len(node.matrix[i])):
                 if (node.matrix[i][j] != state.matrix[i][j]):
                     toggle = False
-        if toggle == True:
+        if toggle:
             return True
     return False
 
@@ -82,32 +84,52 @@ def print_state(state):
         print(i[0] + " " + i[1] + " " + i[2] + " " + i[3] + " " + i[4])
         print("")
 
-def expand_node(state, fringe):
+def expand_node(state, fringe, layer):
     if (state.matrix[state.get_X()][state.get_Y()] == 'dirty'):
         new_state = copy.deepcopy(state)
         new_state.clean_room()
+        new_state.layer = layer
         fringe.append(new_state)
         return
     if (state.get_Y() != 0):
         new_state = copy.deepcopy(state)
         new_state.move_left()
+        new_state.layer = layer
         fringe.append(new_state)
     if (state.get_Y() != 4):
         new_state = copy.deepcopy(state)
         new_state.move_right()
+        new_state.layer = layer
         fringe.append(new_state)
     if (state.get_X() != 0):
         new_state = copy.deepcopy(state)
         new_state.move_up()
+        new_state.layer = layer
         fringe.append(new_state)
     if (state.get_X() != 3):
         new_state = copy.deepcopy(state)
         new_state.move_down()
+        new_state.layer = layer
         fringe.append(new_state)
 
 def uniform_graph_search(state, fringe, num_of_dirty_rooms):
     goal = []
     closed = []
+    fringe.append(state)
+    while len(fringe) > 0:
+        #print("FRINGE COUNT: " + str(len(fringe)))
+        #print("CLOSED COUNT: " + str(len(closed)) + "\n")
+        fringe.sort(key=lambda x: x.total_cost)
+        node = fringe.pop(0)
+        if (node.rooms_cleaned == num_of_dirty_rooms):
+            return node
+        if (not compare_states(node, closed)):
+            closed.append(node)
+            expand_node(node, fringe, -1)
+    return None
+
+def uniform_tree_search(state, fringe, num_of_dirty_rooms):
+    goal = []
     fringe.append(state)
     while True:
         #print("FRINGE COUNT: " + str(len(fringe)))
@@ -119,31 +141,75 @@ def uniform_graph_search(state, fringe, num_of_dirty_rooms):
         if (node.rooms_cleaned == num_of_dirty_rooms):
             goal.append(node)
             return goal
+        expand_node(node, fringe, -1)
+
+def iterative_deepening_search(state, closed, num_of_dirty_rooms, layer):
+    print(layer)
+    state.layer = 0
+    fringe = [state]
+    while len(fringe) > 0:
+        node = fringe.pop(0)
+        if node.layer > layer:
+            break
+        if (node.rooms_cleaned == num_of_dirty_rooms):
+            return node
         if (not compare_states(node, closed)):
-            #print_state(node)
             closed.append(node)
-            expand_node(node, fringe)
+            expand_node(node, fringe, node.layer+1)
+    return iterative_deepening_search(state, closed, num_of_dirty_rooms, layer+1)
+
+def print_info(goal, start_time, end_time):
+    print('   Goal path =>', end=' ')
+    if goal != None:
+        print(goal.path)
+        print('   Cost of traversal => ' + str(goal.get_cost()))
+    else:
+        print('Goal path not found')
+    print('   Execution time => ' + str(end_time-start_time))
 
 
 def main():
-    instance_1 = Env(1, 1)
-    instance_1.set_dirt(0, 1)
-    instance_1.set_dirt(1, 3)
-    instance_1.set_dirt(2, 4)
-    goal = uniform_graph_search(instance_1, [], 3)[0]
-    print_state(goal)
-    print(goal.path)
-    print(goal.get_cost())
+    # print('\nInstance 1 - Uniform Tree Search:')
+    # start = time.time()
+    # instance_1 = Env(1, 1)
+    # instance_1.set_dirt(0, 1)
+    # instance_1.set_dirt(1, 3)
+    # instance_1.set_dirt(2, 4)
+    # goal = uniform_tree_search(instance_1, [], 3)[0]
+    # end = time.time()
+    # print_info(goal, start, end)
 
-    instance_2 = Env(2, 1)
-    instance_2.set_dirt(0, 1)
-    instance_2.set_dirt(1, 0)
-    instance_2.set_dirt(1, 3)
-    instance_2.set_dirt(2, 2)
-    #goal = uniform_graph_search(instance_2, [], 4)[0]
-    #print_state(goal)
-    #print(goal.path)
-    #print(goal.get_cost())
+    # print('\nInstance 1 - Uniform Graph Search:')
+    # start = time.time()
+    # instance_1 = Env(1, 1)
+    # instance_1.set_dirt(0, 1)
+    # instance_1.set_dirt(1, 3)
+    # instance_1.set_dirt(2, 4)
+    # goal = uniform_graph_search(instance_1, [], 3)
+    # end = time.time()
+    # print_info(goal, start, end)
+
+    print('\nInstance 1 - Iterative Deepening Search:')
+    start = time.time()
+    instance_1 = Env(1, 1)
+    instance_1.set_dirt(1, 1)
+    # instance_1.set_dirt(0, 1)
+    # instance_1.set_dirt(1, 3)
+    # instance_1.set_dirt(2, 4)
+    goal = iterative_deepening_search(instance_1, [], 1, 0)
+    end = time.time()
+    print_info(goal, start, end)
+
+    # print('\nInstance 2 - Uniform Graph Search:')
+    # start = time.time()
+    # instance_2 = Env(2, 1)
+    # instance_2.set_dirt(0, 1)
+    # instance_2.set_dirt(1, 0)
+    # instance_2.set_dirt(1, 3)
+    # instance_2.set_dirt(2, 2)
+    # goal = uniform_graph_search(instance_2, [], 4)
+    # end = time.time()
+    # print_info(goal, start, end)
 
 
 
